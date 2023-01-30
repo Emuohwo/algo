83cbfcf9950be487f1f55d71d749b717d6308c61
Emuohwo <warriisaac@outlook.com>
2023-01-30 12:55:33 +0100

    first commit

---
+++ algo.ts
@@ -0,0 +1,17 @@
+// Solution 1
+const rotateArr = (nums: number[] = [], k: number) => [...nums.slice(-k), ...nums.slice(0, -k)];
+console.log(rotateArr([1, 2, 3], 2));
+
+
+
+// Solution 2
+const isMonotone = (arr:number[] = []) => {
+  return arr.every((e, i, a) => {
+    if (i) {
+      return e > a[i - 1]
+    }
+    else {
+      return false
+    }
+  })
+}
